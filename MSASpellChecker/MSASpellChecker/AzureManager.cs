0832e224dee3dd100c636dbcead384f61b0bc1a7
Michael Ji <tuna95@hotmail.co.uk>
2017-07-25 18:42:38 +1200

    Implemented AzureManager and setup for Easy Tables

---
+++ MSASpellChecker/MSASpellChecker/AzureManager.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSASpellChecker
+{
+    public class AzureManager
+    {
+
+        private static AzureManager instance;
+        private MobileServiceClient client;
+        private IMobileServiceTable<EasyTableModel> easyTable;
+
+        private AzureManager()
+        {
+            this.client = new MobileServiceClient("http://mySpellChecker.azurewebsites.net");
+            this.easyTable = this.client.GetTable<EasyTableModel>();
+        }
+
+        public MobileServiceClient AzureClient
+        {
+            get { return client; }
+        }
+
+        public static AzureManager AzureManagerInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AzureManager();
+                }
+
+                return instance;
+            }
+        }
+
+        public async Task AddToHistory(EasyTableModel easyTableModel)
+        {
+            await this.easyTable.InsertAsync(easyTableModel);
+        }
+
+        public async Task DeleteHistory(EasyTableModel easyTableModel)
+        {
+            await this.easyTable.DeleteAsync(easyTableModel);
+        }
+
+        public async Task<List<EasyTableModel>> GetHistory()
+        {
+            return await this.easyTable.ToListAsync();
+        }
+    }
+}
---
+++ MSASpellChecker/MSASpellChecker/EasyTableModel.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSASpellChecker
+{
+    class EasyTableModel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+
+        [JsonProperty(PropertyName = "sentence")]
+        public string sentence { get; set; }
+    }
+}
